79cb9a437550866c51a542febbf0d60f8a713b2b
mulisa innocent <mulisainnocent54@gmail.com>
2025-06-06 19:49:45 +0300

    updates 4

--- resources/views/layouts/website-layout.blade.php
+++ resources/views/layouts/website-layout.blade.php
@@ -369,7 +369,6 @@
             <div class="site-footer-bottom">
                 <div class="container">
                     <div class="row">
-
                         <div class="col-lg-8 col-md-7 col-12">
                             <p class="copyright-text mb-0">Hakimiliki © {{date('Y')}} TAHPC. Haki zote zimehifadhiwa.</p>
                         </div>
